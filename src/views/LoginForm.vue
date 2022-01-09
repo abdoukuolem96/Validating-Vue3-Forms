cdfc3d42fffea0558c68d151972c90c06a7d50f2
kuolem96 <a.melouk@esi-sba.dz>
2022-01-09 18:39:02 +0100

    Lesson 2

--- src/views/LoginForm.vue
+++ src/views/LoginForm.vue
@@ -1,36 +1,38 @@
+/* eslint-disable no-useless-escape */ /* eslint-disable no-useless-escape */
 <template>
   <form @submit.prevent="onSubmit">
-    <BaseInput
-      label="Email"
-      type="email"
-      v-model="email"
-      :error="emailError"
-    />
+    <BaseInput label="Email" type="email" v-model="email" :error="emailError" />
 
-    <BaseInput
-      label="Password"
-      type="password"
-    />
+    <BaseInput label="Password" type="password" />
 
-    <BaseButton
-      type="submit"
-      class="-fill-gradient"
-    >
-    Submit
-    </BaseButton>
+    <BaseButton type="submit" class="-fill-gradient"> Submit </BaseButton>
   </form>
 </template>
 
 <script>
+import { useField } from 'vee-validate'
 export default {
-  setup () {
-    function onSubmit () {
+  setup() {
+    function onSubmit() {
       alert('Submitted')
     }
+    const { value: email, errorMessage: emailError } = useField(
+      'email',
+      (value) => {
+        if (!value) return 'This field is required'
+        const regex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/
+        if (!regex.test(String(value).toLowerCase())) {
+          return 'Please enter a valid email address'
+        }
+        return true
+      }
+    )
 
     return {
-      onSubmit
+      onSubmit,
+      email: email,
+      emailError: emailError,
     }
-  }
+  },
 }
 </script>
